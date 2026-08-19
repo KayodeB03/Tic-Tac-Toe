f17b62693df90b12834057da724dcda786ad88dc
Kayode <194831407+KayodeB03@users.noreply.github.com>
2026-08-19 12:14:37 -0700

    Updated checkWinner() Loop

--- script.js
+++ script.js
@@ -50,78 +50,79 @@ const gameController = (() => {
   function playRound(row, column) {
     if (gameBoard.isSpaceAvailible(row, column)) {
       gameBoard.placeMarker(row, column, currentPlayer.marker);
-      switchTurn();
       console.log(gameBoard.getBoard());
     } else {
       console.log("Space is not availible, Try again.");
     }
+    checkWinner();
+    switchTurn();
   }
+
+  const winConditions = [
+    [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    [
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 0],
+      [2, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [2, 1],
+    ],
+    [
+      [0, 2],
+      [1, 2],
+      [2, 2],
+    ],
+    [
+      [0, 0],
+      [1, 1],
+      [2, 2],
+    ],
+    [
+      [0, 2],
+      [1, 1],
+      [2, 0],
+    ],
+  ];
 
   function checkWinner() {
     const board = gameBoard.getBoard();
     const marker = currentPlayer.marker;
-    if (
-      marker === board[0][0] &&
-      marker === board[0][1] &&
-      marker === board[0][2]
-    ) {
-      console.log(`${currentPlayer.name} wins by top row!`);
-    }
-    if (
-      marker === board[1][0] &&
-      marker === board[1][1] &&
-      marker === board[1][2]
-    ) {
-      console.log(`${currentPlayer.name} wins by middle row!`);
-    }
-    if (
-      marker === board[2][0] &&
-      marker === board[2][1] &&
-      marker === board[2][2]
-    ) {
-      console.log(`${currentPlayer.name} wins by bottom row!`);
-    }
-    if (
-      marker === board[0][0] &&
-      marker === board[1][0] &&
-      marker === board[2][0]
-    ) {
-      console.log(`${currentPlayer.name} wins by left column!`);
-    }
-    if (
-      marker === board[0][1] &&
-      marker === board[1][1] &&
-      marker === board[2][1]
-    ) {
-      console.log(`${currentPlayer.name} wins by middle column!`);
-    }
-    if (
-      marker === board[0][2] &&
-      marker === board[1][2] &&
-      marker === board[2][2]
-    ) {
-      console.log(`${currentPlayer.name} wins by right column!`);
-    }
-    if (
-      marker === board[0][0] &&
-      marker === board[1][1] &&
-      marker === board[2][2]
-    ) {
-      console.log(`${currentPlayer.name} wins by diagonal!`);
-    }
-    if (
-      marker === board[0][2] &&
-      marker === board[1][1] &&
-      marker === board[2][0]
-    ) {
-      console.log(`${currentPlayer.name} wins by diagonal!`);
+    for (const combination of winConditions) {
+      const [a, b, c] = combination;
+
+      // check a, b, and c
+      if (
+        board[a[0]][a[1]] === marker &&
+        board[b[0]][b[1]] === marker &&
+        board[c[0]][c[1]] === marker
+      ) {
+        console.log(`${currentPlayer.name} wins!`);
+        return;
+      }
     }
   }
 
   return {
-    switchTurn,
     playRound,
     checkWinner,
+    switchTurn,
   };
 })();
 
